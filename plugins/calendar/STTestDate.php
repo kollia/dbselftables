37df35e1f891dfc9db53bed782f8541adb701a2d
Alexander Kolli <alexander.kolli@medunigraz.at>
2025-02-17 14:22:57 +0100

    porting back easter_date changes form commit b87852b. It wasn't an source error, but maybe an configuration error

--- plugins/calendar/STTestDate.php
+++ plugins/calendar/STTestDate.php
@@ -125,12 +125,12 @@ class STtDate
                     "day" => "06",
                     "month" => "01"
                 ),
-                array( "name" => "Aschermittwoch",      "public" => false, "date" => "easter", "day" => -45 ), // check 2024 am 14.02.
-                array( "name" => "Palmsonntag",         "public" => false, "date" => "easter", "day" =>  -7 ), // check 2024 am 24.03.
-                array( "name" => "Gründonnerstag",		"public" => false, "date" => "easter", "day" =>  -2 ), // check 2024 am 28.03.
-                array( "name" => "Karfreitag",		    "public" => false, "date" => "easter", "day" =>  -1 ), // check 2024 am 29.03.
-                array( "name" => "Ostersonntag",		"public" => true,  "date" => "easter", "day" =>   1 ), // check 2024 am 31.03.
-                array( "name" => "Ostermontag",		    "public" => true,  "date" => "easter", "day" =>   2 ), // check 2024 am 01.04.
+                array( "name" => "Aschermittwoch",      "public" => false, "date" => "easter", "day" => -46 ),
+                array( "name" => "Palmsonntag",         "public" => false, "date" => "easter", "day" =>  -7 ),
+                array( "name" => "Gründonnerstag",		"public" => false, "date" => "easter", "day" =>  -3 ),
+                array( "name" => "Karfreitag",		    "public" => false, "date" => "easter", "day" =>  -2 ),
+                array( "name" => "Ostersonntag",		"public" => true,  "date" => "easter", "day" =>   0 ),
+                array( "name" => "Ostermontag",		    "public" => true,  "date" => "easter", "day" =>   1 ),
                 array(
                     "name" => "Staatsfeiertag",
                     "public" => true,
@@ -138,10 +138,10 @@ class STtDate
                     "day" => "01",
                     "month" => "05"
                 ),
-                array( "name" => "Christi Himmelfahrt", "public" => true,  "date" => "easter", "day" =>  40 ), // check 2024 am 09.05.
-                array( "name" => "Pfingstsonntag",		"public" => true,  "date" => "easter", "day" =>  50 ), // check 2024 am 19.05.
-                array( "name" => "Pfingstmontag",		"public" => true,  "date" => "easter", "day" =>  51 ), // check 2024 am 20.05.
-                array( "name" => "Fronleichnam",		"public" => true,  "date" => "easter", "day" =>  61 ), // check 2024 am 30.05.
+                array( "name" => "Christi Himmelfahrt", "public" => true,  "date" => "easter", "day" =>  39 ),
+                array( "name" => "Pfingstsonntag",		"public" => true,  "date" => "easter", "day" =>  49 ),
+                array( "name" => "Pfingstmontag",		"public" => true,  "date" => "easter", "day" =>  50 ),
+                array( "name" => "Fronleichnam",		"public" => true,  "date" => "easter", "day" =>  60 ),
                 array(
                     "name" => "Mariä Himmelfahrt",
                     "public" => true,
@@ -208,6 +208,10 @@ class STtDate
             );
         }
     }
+    protected function getEasterTimestamp($year) : int
+    {
+        return easter_date($year);
+    }
     protected function defineSpecificDays_forYear(string $year)
     {
         global $global_selftable_dateFormat_struct;
@@ -216,7 +220,14 @@ class STtDate
 
         
         $locale= $global_selftable_dateFormat_struct['locale'];
-        $easter_timestamp= easter_date($year);        
+        $easter_timestamp= $this->getEasterTimestamp($year);
+        if(STCheck::isDebug("easter.date"))
+        {
+            // by my last tests PHP version 8.3 give me Saturday as easter date
+            // PHP version 8.1 does right, maybe there was different settings from ubuntu 22.04 to 24.04
+            echo "easter date for ".$year." is ".date("l Y-m-d H:i:s", $easter_timestamp)."<br>";
+            echo "current time zone is ".date_default_timezone_get()."<br>";
+        }
         $defined= array();
         foreach($global_selftable_specific_days[$locale] as $day)
         {
